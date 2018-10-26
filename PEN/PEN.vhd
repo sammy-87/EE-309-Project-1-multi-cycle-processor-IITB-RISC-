08c585ee0330b7991a1eab109a55f1fd98282998
tanmay1598 <tanmaypatil1598@gmail.com>
2018-10-26 17:11:47 +0530

    modified tp update logic

--- PEN/PEN.vhd
+++ PEN/PEN.vhd
@@ -16,45 +16,46 @@ architecture behave of PEN is
 
 begin 
 
-	
+	--tp <= not(penin(0) or penin(1) or penin(2) or penin(3) or penin(4) or penin(5) or penin(6) or penin(7));
 
 	proc_pen : process(penin)
 	begin
+		variable next_var : std_logic_vector(7 downto 0);
+		
 		if (penin(7) = '1') then 
 			penout <= "111";
-			pennext <= ("01111111" and penin);
-			tp <= '0';
+			next_var := ("01111111" and penin);
 		elsif (penin(6) = '1') then 
 			penout <= "110";
-			pennext <= ("00111111" and penin);
-			tp <= '0';
+			next_var := ("00111111" and penin);
 		elsif (penin(5) = '1') then 
 			penout <= "101";
-			pennext <= ("00011111" and penin);
-			tp <= '0';
+			next_var := ("00011111" and penin);
 		elsif (penin(4) = '1') then 
 			penout <= "100";
-			pennext <= ("00001111" and penin);
-			tp <= '0';
+			next_var := ("00001111" and penin);
 		elsif (penin(3) = '1') then 
 			penout <= "011";
-			pennext <= ("00000111" and penin);
-			tp <= '0';
+			next_var := ("00000111" and penin);
 		elsif (penin(2) = '1') then 
 			penout <= "010";
-			pennext <= ("00000011" and penin);
-			tp <= '0';
+			next_var := ("00000011" and penin);
 		elsif (penin(1) = '1') then 
 			penout <= "001";
-			pennext <= ("00000001" and penin);
-			tp <= '0';
+			next_var := ("00000001" and penin);
 		else
 			penout <= "000";
-			pennext <= ("00000000" and penin);
-			tp <= '1';
+			next_var := ("00000000" and penin);
 		end if;
+			
+		pennext <= next_var;
+		
+		if next_var = "00000000" then
+			tp <= '1';
+		else
+			tp <= '0';
+		end if;
+
 	end process proc_pen;
-	
-	
-	
+
 end behave;
